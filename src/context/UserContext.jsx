de329f0253327038bac3cd9052d33a69b6ef2cb6
Balamurugan-k2005 <balamurugannbr500@gmail.com>
2025-04-08 21:35:32 +0530

    url changed

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -4,34 +4,34 @@ import axios from "axios";
 const userContext = React.createContext();
 
 const UserContext = ({ children }) => {
-    const [userLoading, setUserLoading] = useState(true);
-    const [userError, setUserError] = useState({ status: false, message: "" });
-    const [user, setUser] = useState({});
+  const [userLoading, setUserLoading] = useState(true);
+  const [userError, setUserError] = useState({ status: false, message: "" });
+  const [user, setUser] = useState({});
 
-    const handleFetchMe = async () => {
-        setUserLoading(true);
-        try {
-            const response = await axios.get(
-                `https://full-stack-job-portal-server.vercel.app/api/v1/auth/me`,
-                { withCredentials: true }
-            );
-            setUserError({ status: false, message: "" });
-            setUser(response?.data?.result);
-        } catch (error) {
-            setUserError({ status: true, message: error?.message });
-            setUser({ status: false });
-        }
-        setUserLoading(false);
-    };
+  const handleFetchMe = async () => {
+    setUserLoading(true);
+    try {
+      const response = await axios.get(
+        `https://mern-job-portal-server-kappa.vercel.app/api/v1/auth/me`,
+        { withCredentials: true }
+      );
+      setUserError({ status: false, message: "" });
+      setUser(response?.data?.result);
+    } catch (error) {
+      setUserError({ status: true, message: error?.message });
+      setUser({ status: false });
+    }
+    setUserLoading(false);
+  };
 
-    useEffect(() => {
-        handleFetchMe();
-    }, []);
+  useEffect(() => {
+    handleFetchMe();
+  }, []);
 
-    const passing = { userLoading, userError, user, handleFetchMe };
-    return (
-        <userContext.Provider value={passing}>{children}</userContext.Provider>
-    );
+  const passing = { userLoading, userError, user, handleFetchMe };
+  return (
+    <userContext.Provider value={passing}>{children}</userContext.Provider>
+  );
 };
 
 const useUserContext = () => useContext(userContext);
